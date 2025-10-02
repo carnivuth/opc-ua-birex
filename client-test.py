64b79bbe5b82d7b0e25894cd728c2a4b8154a289
carnivuth <matti200042@gmail.com>
2025-10-02 12:30:10 +0200

    first succesfull connection to birex nicon machine

--- client-test.py
+++ client-test.py
@@ -1,10 +1,26 @@
 import asyncio
 from asyncua import Client
 
+namespace = "http://examples.freeopcua.github.io"
+
 async def main():
-    async with Client(url='opc.tcp://localhost:4840/freeopcua/server/') as client:
-        while True:
-            # Do something with client
-            node = client.get_node('ns=2')
-            value = await node.read_value()
+    client = Client(url='opc.tcp://192.168.102.10:4840')
+    """
+        Set SecureConnection mode.
+        :param string: Mode format ``Policy,Mode,certificate,private_key[,server_certificate]``
+        where:
+        - ``Policy`` is ``Basic256Sha256``, ``Aes128Sha256RsaOaep`` or ``Aes256Sha256RsaPss``
+        - ``Mode`` is ``Sign`` or ``SignAndEncrypt``
+        - ``certificate`` and ``server_certificate`` are paths to ``.pem`` or ``.der`` files
+        - ``private_key`` may be a path to a ``.pem`` or ``.der`` file or a conjunction of ``path``::``password`` where
+          ``password`` is the private key password.
+        Call this before connect()
+        """
+    client.application_uri= 'urn:serperior:UnifiedAutomation:UaExpert'
+    await client.set_security_string("Aes128Sha256RsaOaep,SignAndEncrypt,certs/own/uaexpert.der,certs/own/uaexpert_key.pem,certs/server/nikonslm.birex.der")
+    await client.connect()
+    node = client.get_node('ns=5;i=6010')
+    value = await node.read_value()
+    print(f"Value of MyVariable : {value}")
+
 asyncio.run(main())
